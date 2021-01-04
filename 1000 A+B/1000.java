96c3ed26b184abfb7f4c5483092ef433cefa72f1
kiheyunkim <kiheyunKim@gmail.com>
2021-01-04 21:28:55 +0900

    1000번 풀이

---
+++ 1000 A+B/1000.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * IDE : IntelliJ IDEA
+ * Created by kiheyunkim@gmail.com on 2021-01-04
+ * Github : http://github.com/kiheyunkim
+ * Comment :
+ */
+public class Main {
+    static public void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String inputsLine = bufferedReader.readLine();
+
+        String[] inputSplits = inputsLine.split(" ");
+        System.out.println(Integer.parseInt(inputSplits[0]) + Integer.parseInt(inputSplits[1]));
+    }
+}
