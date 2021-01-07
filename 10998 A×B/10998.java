9c3d580d267e1308d347a1da17c49c21db092ea0
kiheyunkim <kiheyunKim@gmail.com>
2021-01-07 21:20:57 +0900

    10998 axb 추가

---
+++ 10998 A×B/10998.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static public void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String inputs = bufferedReader.readLine();
+        String[] inputSplits = inputs.split(" ");
+
+        System.out.println(Integer.parseInt(inputSplits[0]) * Integer.parseInt(inputSplits[1]));
+    }
+}
