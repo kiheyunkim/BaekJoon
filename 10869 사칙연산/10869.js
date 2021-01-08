edb905671d8f93602733177a7be73b4ba45d6f3b
kiheyunkim <kiheyunKim@gmail.com>
2021-01-08 20:21:09 +0900

    10869 사칙연산 추가

---
+++ 10869 사칙연산/10869.js
@@ -0,0 +1,7 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+console.log(parseInt(input[0]) + parseInt(input[1]));
+console.log(parseInt(input[0]) - parseInt(input[1]));
+console.log(parseInt(input[0]) * parseInt(input[1]));
+console.log(parseInt(parseInt(input[0]) / parseInt(input[1])));
+console.log(parseInt(input[0]) % parseInt(input[1]));
