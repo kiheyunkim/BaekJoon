b6ca3899a64b4004c0f7f4e19a0bbf991d6202d6
kiheyunkim <kiheyunKim@gmail.com>
2021-01-17 14:47:46 +0900

    2438 별찍기 - 1 추가

---
+++ 2438 별 찍기 - 1/2438.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    static public void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(bufferedReader.readLine());
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                bufferedWriter.write('*');
+            }
+            bufferedWriter.write('\n');
+        }
+
+
+        bufferedWriter.flush();
+    }
+}
---
+++ 2438 별 찍기 - 1/2438.js
@@ -0,0 +1,13 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let n = parseInt(input[0]);
+
+let answer = '';
+for (let i = 1; i < n + 1; ++i) {
+    for (let j = 0; j < i; ++j) {
+        answer += '*';
+    }
+    answer += '\n';
+}
+
+console.log(answer);
